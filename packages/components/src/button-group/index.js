5cfb86ceb89bfa35bd8ce32d050a5e275bc6415b
Jon Q <hello@jonquach.com>
2020-04-02 17:17:04 -0400

    Creating initial BoxControl component

--- packages/components/src/button-group/index.js
+++ packages/components/src/button-group/index.js
@@ -3,10 +3,15 @@
  */
 import classnames from 'classnames';
 
-function ButtonGroup( { className, ...props } ) {
+/**
+ * WordPress dependencies
+ */
+import { forwardRef } from '@wordpress/element';
+
+function ButtonGroup( { className, ...props }, ref ) {
 	const classes = classnames( 'components-button-group', className );
 
-	return <div { ...props } className={ classes } role="group" />;
+	return <div { ...props } className={ classes } ref={ ref } role="group" />;
 }
 
-export default ButtonGroup;
+export default forwardRef( ButtonGroup );
