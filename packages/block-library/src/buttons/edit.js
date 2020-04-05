55a94239cc8d1845a2e5b07fb56d3fcec5850820
nfmohit-wpmudev <nfmohit49@gmail.com>
2020-04-05 11:48:41 +0600

    Attempted the solution with attributes and a RadioControl

--- packages/block-library/src/buttons/edit.js
+++ packages/block-library/src/buttons/edit.js
@@ -1,10 +1,13 @@
 /**
  * WordPress dependencies
  */
+import { __ } from '@wordpress/i18n';
 import {
 	__experimentalAlignmentHookSettingsProvider as AlignmentHookSettingsProvider,
 	InnerBlocks,
+	InspectorControls,
 } from '@wordpress/block-editor';
+import { PanelBody, RadioControl } from '@wordpress/components';
 
 /**
  * Internal dependencies
@@ -19,16 +22,38 @@ const alignmentHooksSetting = {
 	isEmbedButton: true,
 };
 
-function ButtonsEdit( { className } ) {
+function ButtonsEdit( { className, attributes, setAttributes } ) {
+	const { blockDirection } = attributes;
+
 	return (
-		<div className={ className }>
+		<div className={ `${ className } is-direction-${ blockDirection }` }>
 			<AlignmentHookSettingsProvider value={ alignmentHooksSetting }>
 				<InnerBlocks
 					allowedBlocks={ ALLOWED_BLOCKS }
 					template={ BUTTONS_TEMPLATE }
-					__experimentalMoverDirection="horizontal"
+					__experimentalMoverDirection={ blockDirection }
 				/>
 			</AlignmentHookSettingsProvider>
+			<InspectorControls>
+				<PanelBody title={ __( 'Buttons Settings' ) }>
+					<RadioControl
+						label={ __( 'Direction' ) }
+						selected={ blockDirection }
+						options={ [
+							{ label: __( 'Horizontal' ), value: 'horizontal' },
+							{
+								label: __( 'Vertical' ),
+								value: 'vertical',
+							},
+						] }
+						onChange={ ( value ) =>
+							setAttributes( {
+								blockDirection: value,
+							} )
+						}
+					/>
+				</PanelBody>
+			</InspectorControls>
 		</div>
 	);
 }
